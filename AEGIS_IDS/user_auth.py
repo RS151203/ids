842992d26c9da60f7cb6381d35076ee200d9d2b1
Rajat Sabat <90056661+RS151203@users.noreply.github.com>
2024-03-25 18:11:34 +0530

    Update user_auth.py

--- AEGIS_IDS/user_auth.py
+++ AEGIS_IDS/user_auth.py
@@ -3,7 +3,6 @@ import hashlib
 
 
 def create_connection(db_file):
-    """Create a database connection to the SQLite database"""
     conn = None
     try:
         conn = sqlite3.connect(db_file)
@@ -14,12 +13,10 @@ def create_connection(db_file):
 
 
 def hash_password(password):
-    """Hash the password using SHA-256"""
     return hashlib.sha256(password.encode()).hexdigest()
 
 
 def add_user(conn, username, password):
-    """Add a new user with hashed password to the database"""
     hashed_password = hash_password(password)
     try:
         cursor = conn.cursor()
@@ -34,7 +31,6 @@ def add_user(conn, username, password):
 
 
 def check_user(conn, username, password):
-    """Check if a user with the given username and password exists"""
     hashed_password = hash_password(password)
     try:
         cursor = conn.cursor()
@@ -45,5 +41,58 @@ def check_user(conn, username, password):
             return True
         else:
             return False
+    except sqlite3.Error as e:
+        print(e)
+
+
+def change_user_credentials(conn, current_username, current_password, new_username=None, new_password=None):
+    try:
+        cursor = conn.cursor()
+        username = current_username
+        if new_username:
+            cursor.execute("UPDATE user_authentication SET username=? WHERE username=? AND password=?",
+                           (new_username, current_username, hash_password(current_password)))
+            username = new_username
+        if new_password:
+            hashed_new_password = hash_password(new_password)
+            if username != current_username:
+                cursor.execute("UPDATE user_authentication SET password=? WHERE username=? AND password=?",
+                               (hashed_new_password, username, hash_password(current_password)))
+            else:
+                cursor.execute("UPDATE user_authentication SET password=? WHERE username=? AND password=?",
+                               (hashed_new_password, current_username, hash_password(current_password)))
+        conn.commit()
+        return username
+    except sqlite3.Error as e:
+        print(e)
+
+
+def add_email(conn, username, email):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("UPDATE user_authentication SET email=? WHERE username=?", (email, username))
+        conn.commit()
+    except sqlite3.Error as e:
+        print(e)
+
+
+def get_email(conn, username):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("SELECT email FROM user_authentication WHERE username=?", (username,))
+        result = cursor.fetchone()
+        if result:
+            emails_text = result[0]
+            return emails_text
+    except sqlite3.Error as e:
+        print(e)
+
+
+def delete_email(conn, username):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("UPDATE user_authentication SET email=NULL WHERE username=?", (username,))
+        conn.commit()
+        print("Emails deleted successfully for username:", username)
     except sqlite3.Error as e:
         print(e)
